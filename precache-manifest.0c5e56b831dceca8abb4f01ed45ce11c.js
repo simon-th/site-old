baa9f19323d78c9767a29aed3d40b21da9b217e2
simon-th <simon.thoque@gmail.com>
2019-12-16 00:40:22 -0600

    Updates

---
+++ precache-manifest.0c5e56b831dceca8abb4f01ed45ce11c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e910bd6163928bd3f422e21fc9f89dbf",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "be45f248cd51897a4fab",
+    "url": "/site/static/css/main.f5d68ed6.chunk.css"
+  },
+  {
+    "revision": "4c1f5e244fcb7b0e46ad",
+    "url": "/site/static/js/2.7125f1e5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/site/static/js/2.7125f1e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be45f248cd51897a4fab",
+    "url": "/site/static/js/main.800bdecc.chunk.js"
+  },
+  {
+    "revision": "f34c3a701b297cdc34ce",
+    "url": "/site/static/js/runtime-main.a0e87784.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/site/static/media/logo.5d5d9eef.svg"
+  }
+]);
